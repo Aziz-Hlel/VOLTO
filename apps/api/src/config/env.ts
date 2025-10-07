0df4e87a8150b157f59ea737780804e7d74ec801
Mohamed Aziz Ben Hlel <m.aziz.hlel@gmail.com>
2025-10-07 16:47:02 +0200

    replace s3 access with access key

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -18,8 +18,8 @@ export const envSchema = z.object({
   DATABASE_URL: z.string(),
 
   AWS_REGION: z.string(),
-  // AWS_ACCESS_KEY_ID: z.string(),
-  // AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
   AWS_S3_BUCKET: z.string(),
   AWS_CLOUDFRONT_URL: z.string(),
 
