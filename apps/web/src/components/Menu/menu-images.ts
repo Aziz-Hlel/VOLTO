29e448863b0d9434039eee8f85262e0966eb241d
Mohamed Aziz Ben Hlel <m.aziz.hlel@gmail.com>
2025-12-03 16:51:07 +0100

    updated menu volto

--- apps/web/src/components/Menu/menu-images.ts
+++ apps/web/src/components/Menu/menu-images.ts
@@ -6,10 +6,7 @@ export const menuImages: { [key in MenuCarouselTypes]: string[] } = {
     "/img/menu/cards/menu/V2.2_page-0002.jpg",
     "/img/menu/cards/menu/V2.2_page-0003.jpg",
     "/img/menu/cards/menu/V2.2_page-0004.jpg",
-    "/img/menu/cards/menu/V2.2_page-0005.jpg",
-    "/img/menu/cards/menu/V2.2_page-0006.jpg",
-    "/img/menu/cards/menu/V2.2_page-0007.jpg",
-    "/img/menu/cards/menu/V2.2_page-0008.jpg",
+    "/img/menu/cards/menu/V2.2_page-0005.jpg"
   ],
   Cocktails: [
     "/img/menu/cards/cocktails/volto_beverages_page-0001.jpg",
