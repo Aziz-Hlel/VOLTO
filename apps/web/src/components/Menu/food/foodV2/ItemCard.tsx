979613775dce7e35b4c4f8053f946837e2484da4
Tigana <m.aziz.hlel@gmail.com>
2026-01-29 16:35:20 +0100

    fixed item card layout

--- apps/web/src/components/Menu/food/foodV2/ItemCard.tsx
+++ apps/web/src/components/Menu/food/foodV2/ItemCard.tsx
@@ -2,21 +2,13 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import type { FoodItem } from "../foodData";
-import { BookText, TriangleAlert, CircleDollarSign } from "lucide-react";
 
 const ItemCard = ({
   item,
@@ -27,24 +19,23 @@ const ItemCard = ({
 }) => {
   return (
     <Dialog open={item !== null} onOpenChange={() => onItemSelect(null)}>
-      <DialogContent className=" p-0 lg:mt-0 rounded-xl max-h-[90vh] z-[1000]">
+      <DialogContent className=" p-0 lg:mt-0 rounded-xl max-h-[90dvh] z-[1000]">
         {item && (
           <Card className=" p-0 m-0  max-w-2xl h-full bg-white/10 backdrop-blur-md border border-white/20 shadow-lg">
             <CardContent className="flex flex-col h-fit p-0 m-0  ">
               <img
                 src={item.src}
                 alt={item.title}
-                className=" w-full h-[32rem] lg:h-[32rem] rounded-t-xl  object-cover  "
+                className=" w-full h-[65dvh] rounded-t-xl  object-cover  "
               />
-              <div className="px-0.5 sm:px-4 overflow-y-auto  max-h-[calc(90vh-32rem)] lg:max-h-[calc(90vh-32rem)] h-fit flex flex-col py-4  space-y-4 overscroll-contain scrollbar-thin scrollbar-thumb-neutral-300 scrollbar-track-transparent hover:scrollbar-thumb-neutral-400">
-                <h2 className=" font-semibold font-['EB_Garamond'] uppercase text-gray-900 text-2xl  text-center  ">
+              <div className="px-0.5 sm:px-4 overflow-y-auto max-h-[25dvh] h-fit flex flex-col py-4  space-y-2 overscroll-contain scrollbar-thin scrollbar-thumb-neutral-300 scrollbar-track-transparent hover:scrollbar-thumb-neutral-400">
+                <DialogTitle className="font-semibold font-['EB_Garamond'] uppercase text-gray-900 text-2xl  text-center">
                   {item.title}
-                </h2>
-
+                </DialogTitle>
                 <div className=" flex flex-col items-center space-y-2 h-fit">
-                  <span className=" font-['Cinzel']  text-gray-900 text-md pl-2 h-fit text-center tracking-tight leading-tight">
+                  <DialogDescription className="font-['Cinzel'] text-gray-900 text-md pl-2 h-fit text-center tracking-tight leading-tight">
                     {item.description}
-                  </span>
+                  </DialogDescription>
                   <span className="font-['Cinzel'] text-gray-900 text-2xl pl-2 h-fit text-center">
                     {item.price}
                   </span>
