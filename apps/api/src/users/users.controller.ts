e18c74e03452a24dbd409bd6e52595b347740367
Tigana <m.aziz.hlel@gmail.com>
2026-09-22 12:40:23 +0100

    ...

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common';
 import { Role } from '@prisma/client';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAccessGuard } from 'src/auth/guards/jwt.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { AuthUser } from './Dto/AuthUser';
 import { GetUsersQuery } from './Dto/get-users-query';
 import { ListTransactionHistoryCursorParam } from './Dto/list-transaction-history-param.dto';
 import { UsersService } from './users.service';
@@ -22,9 +24,12 @@ export class UsersController {
 
   @UseGuards(JwtAccessGuard)
   @HttpCode(200)
-  @Get('/transactions')
-  async getUserTransactions(@Query() query: ListTransactionHistoryCursorParam) {
-    const response = await this.usersService.ListAllTransactionHistory(query);
+  @Get('/transactions/me')
+  async getUserTransactions(
+    @CurrentUser() user: AuthUser,
+    @Query() query: ListTransactionHistoryCursorParam,
+  ) {
+    const response = await this.usersService.ListMyUserTransactionHistory(user, query);
     return response;
   }
 }
